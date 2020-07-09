473b7cdab5a615b9cda18f305e6de96c08d056bc
Jacques Marneweck <jacques@siberia.co.za>
2020-07-09 21:13:45 +0000

    Prep for dev

---
+++ README.md
@@ -0,0 +1,16 @@
+# Lead Generator
+
+
+
+
+---
+
+## Authors
+
+ * Jacques Marneweck <<jacques@siberia.co.za>>
+
+---
+
+## License
+
+[Proprietary](LICENSE)
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+/**
+ * Lead Generator.
+ *
+ * @author    Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2018-2020 Jacques MArneweck.  All rights strictly reserved.
+ */
+
+require_once __DIR__.'/../src/bootstrap.php';
+
+$app->run();
