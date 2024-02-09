73f17290ca2af6ebc20cb7b68e4ad3d5658399f6
Jacques Marneweck <jacques@siberia.co.za>
2024-02-09 07:00:27 +0000

    Save config

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * @author    Jacques Marneweck <jacques@powertrip.co.za>
+ * @copyright 2020 Jacques Marneweck.  All rights strictly reserved.
+ */
+
+return [
+    'ipstack' => 'ea48ebeef1632e19c22ebab94bdc2349',
+];
